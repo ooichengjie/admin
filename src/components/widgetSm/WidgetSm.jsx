1604692f14db496e4b80781ba29567770cca4a68
ooi cheng jie <ooichengjie@gmail.com>
2022-04-27 16:05:32 +0800

    First commit

---
+++ src/components/widgetSm/WidgetSm.jsx
@@ -0,0 +1,45 @@
+import "./widgetSm.css"
+import { Visibility } from "@material-ui/icons";
+
+export default function WidgetSm() {
+  return (
+    <div className="widgetSm">
+        <span className="widgetSmTitle">New Join Members</span>
+            <ul className="widgetSmList">
+              <li className="widgetSmListItem">
+                <img src="https://images.pexels.com/photos/3992656/pexels-photo-3992656.png?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" className="widgetSmImg"/>
+                <div className="widgetSmUser">
+                  <span className="widgetSmUsername">Black Natalie</span>
+                  <span className="widgetSmUserTitle">Full stack developer</span>
+                </div>
+                <button className="widgetSmButton">
+                    <Visibility />
+                    Display
+                </button>
+              </li>
+              <li className="widgetSmListItem">
+                <img src="https://images.pexels.com/photos/3992656/pexels-photo-3992656.png?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" className="widgetSmImg"/>
+                <div className="widgetSmUser">
+                  <span className="widgetSmUsername">Black Natalie</span>
+                  <span className="widgetSmUserTitle">Full stack developer</span>
+                </div>
+                <button className="widgetSmButton">
+                    <Visibility />
+                    Display
+                </button>
+              </li>
+              <li className="widgetSmListItem">
+                <img src="https://images.pexels.com/photos/3992656/pexels-photo-3992656.png?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" className="widgetSmImg"/>
+                <div className="widgetSmUser">
+                  <span className="widgetSmUsername">Black Natalie</span>
+                  <span className="widgetSmUserTitle">Full stack developer</span>
+                </div>
+                <button className="widgetSmButton">
+                    <Visibility className="widgetSmIcon"/>
+                    Display
+                </button>
+              </li>
+            </ul>
+    </div>
+  )
+}
---
+++ src/pages/newuser/NewUser.jsx
@@ -0,0 +1,50 @@
+import "./newUser.css"
+
+export default function NewUser() {
+  return (
+    <div className="newUser">
+        <h1 className="newUserTitle">New User</h1>
+        <form className="newUserForm">
+            <div className="newUserItem">
+                <label>Username</label>
+                <input type="text" placeholder="Username"/>
+            </div>
+            <div className="newUserItem">
+                <label>Full name</label>
+                <input type="text" placeholder="Full name"/>
+            </div>
+            <div className="newUserItem">
+                <label>Email</label>
+                <input type="email" placeholder="password"/>
+            </div>
+            <div className="newUserItem">
+                <label>Phone</label>
+                <input type="text" placeholder="_+1 123 456 78"/>
+            </div>
+            <div className="newUserItem">
+                <label>Address</label>
+                <input type="text" placeholder="New York | USA"/>
+            </div>
+            <div className="newUserItem">
+                <label>Gender</label>
+                <div className="newUserGender">
+                    <input type="radio" name="gender" id="male" value="male"/>
+                    <label for="male">Male</label>
+                    <input type="radio" name="gender" id="female" value="female"/>
+                    <label for="female">Female</label>
+                    <input type="radio" name="gender" id="other" value="other"/>
+                    <label for="Other"/>
+                </div>
+            </div>
+            <div className="newUserItem">
+            <label>Active</label>
+            <select className="newUserSelect" name="active" id="active">
+                <option value="yes">Yes</option>
+                <option value="no">No</option>         
+            </select>
+            </div>    
+            <button className="newUserButton">Create</button>
+        </form>
+    </div>
+  )
+}
